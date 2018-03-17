2d5b11176092e92abf52545feb7e1a2cfc9cf7dc
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-17 01:16:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function takeANumber(katzDeliLine, customerName) {
 function nowServing(katzDeliLine) {
   
   if(katzDeliLine.length !== 0) {
-    
+    return katzDeliLine.shift();
   } else {
-    
+    return "There is nobody waiting to be served!"
   }
   
 }
